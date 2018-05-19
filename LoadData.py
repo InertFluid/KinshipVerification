ca7420da5e142929fbe24a745114920600c68e11
Joel Estibeiro <joelestibeiro7@gmail.com>
2018-05-19 14:45:55 +0530

    Update LoadData.py
    Updated the model.

--- LoadData.py
+++ LoadData.py
@@ -91,25 +91,36 @@ for i in range(0, 5):
     X_Test[i] = X[(i+4)%5]
     Y_Test[i] = Y[(i+4)%5]
 
-WTInit = RandomNormal(mean=0.0, stddev=0.01, seed=5)
+#Model Definition
 model=Sequential()
-model.add(Convolution2D(16, (5, 5), input_shape=(64, 64, 6), activation="relu", kernel_initializer=WTInit, bias_initializer="zeros"))
+model.add(Convolution2D(16, (5, 5), input_shape=(64, 64, 6)))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Convolution2D(64, (5, 5), activation="relu", kernel_initializer=WTInit, bias_initializer="zeros"))
+model.add(Convolution2D(64, (5, 5)))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Convolution2D(128, (5, 5), activation="relu", kernel_initializer=WTInit, bias_initializer="zeros"))
+model.add(Convolution2D(256, (5, 5)))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
 
 model.add(Flatten())
+model.add(Dropout(0.5))
+    
+model.add(Dense(640))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
 
-model.add(Dense(640, activation="relu", kernel_initializer=WTInit, bias_initializer="zeros"))
-model.add(Dense(2, activation="softmax", kernel_initializer=WTInit, bias_initializer="zeros"))
+model.add(Dropout(0.5))
+model.add(Dense(2))
+model.add(Activation('softmax'))
 
-sgd = SGD(lr= 0.01, momentum=0.9, decay=0.005)
-model.compile(optimizer=sgd, loss="categorical_crossentropy", metrics=['accuracy'])
+sgd = SGD(lr=0.0001, momentum=0.9, decay=0.005)
+model.compile(optimizer='sgd', loss="categorical_crossentropy", metrics=['accuracy'])
 print(model.summary())
-
 
 X_Train_1 = np.reshape(list(X_Train_1), (X_Train_1.shape[0], 64, 64, 6))
 Y_Train_1 = keras.utils.to_categorical(Y_Train_1, 2)
